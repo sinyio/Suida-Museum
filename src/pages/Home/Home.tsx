4aae40634624b27ffff9e6ba541bed41065918dd
Ilya Sinyakin <arqu1t@mail.ru>
2024-07-21 00:11:19 +0300

    Видео, шрифты

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,17 @@
 import styles from './Home.module.css'
+import video from '../../assets/Suida.mp4'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <main>
+      <section>
+        <div className={styles.heroWrapper}>
+          <h1 className={styles.title}>Музей-Усадьба Суйда</h1>
+          <p className={styles.description}>Кстати,  представители современных социальных резервов лишь добавляют фракционных разногласий и призваны к ответу.</p>
+        </div>
+        <video className={styles.video} src={video} muted autoPlay loop></video>
+      </section>
+    </main>
   )
 }
 
