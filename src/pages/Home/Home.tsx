1b01637cf61077937503cc19152bbe6b34bc6f4f
Ilya Sinyakin <arqu1t@mail.ru>
2024-07-23 20:06:35 +0300

    Вынес секцию

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,18 +1,9 @@
-import styles from './Home.module.css'
-import video from '../../assets/Suida.mp4'
+import Hero from "./Hero/Hero"
 
 const Home = () => {
   return (
     <main>
-      <section>
-        <div className={styles.heroWrapper}>
-          <h1 className={styles.title}>Музей-усадьба Суйда</h1>
-          <p className={styles.description}>Кстати,  представители современных социальных резервов лишь добавляют фракционных разногласий и призваны к ответу.</p>
-        </div>
-        <div className={styles.videoWrapper}>
-          <video className={styles.video} src={video} muted autoPlay loop></video>
-        </div>
-      </section>
+      <Hero />
     </main>
   )
 }
