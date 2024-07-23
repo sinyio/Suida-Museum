0c0fbd11a78b9888204171c1e959ded7fd008531
Ilya Sinyakin <arqu1t@mail.ru>
2024-07-23 17:52:31 +0300

    Фикс лого

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,13 +2,14 @@ import { FC } from "react"
 import styles from './Header.module.css'
 import { useTranslation } from "../../hooks/useTranslation/useTranslation"
 import LanguageSwitch from "../LanguageSwitch/LanguageSwitch"
+import logo from '../../assets/logo.png'
 
 const Header: FC = () => {
   const { text } = useTranslation('navigation')
 
   return (
     <header className={styles.header}>
-      <img src="src\assets\suida-logo.svg" aria-hidden="true" className={styles.leftHeaderItem}/>
+      <img src={logo} aria-hidden="true" className={styles.leftHeaderItem}/>
       <nav>
         <ul className={styles.navList}>
           <li>{text?.tickets}</li>
