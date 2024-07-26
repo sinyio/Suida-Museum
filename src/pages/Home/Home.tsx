b354a06e1454db8e1af2c793f9a27e41725aff09
Dmitry Smotryaev <dmitry.smotryaev@gmail.com>
2024-07-26 22:46:13 +0300

    Добавил перевод для hero main

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,16 @@
 import styles from './Home.module.css'
 import video from '../../assets/Suida.mp4'
+import { useTranslation } from '../../hooks/useTranslation/useTranslation'
 
 const Home = () => {
+  const { text } = useTranslation('hero')
+
   return (
     <main>
       <section>
         <div className={styles.heroWrapper}>
-          <h1 className={styles.title}>Музей-усадьба Суйда</h1>
-          <p className={styles.description}>Кстати,  представители современных социальных резервов лишь добавляют фракционных разногласий и призваны к ответу.</p>
+          <h1 className={styles.title}>{text?.header}</h1>
+          <p className={styles.description}>{text?.description}</p>
         </div>
         <div className={styles.videoWrapper}>
           <video className={styles.video} src={video} muted autoPlay loop></video>
