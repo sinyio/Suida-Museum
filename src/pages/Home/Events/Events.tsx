617a185966cb4a48a7d90fd0751be65f4929e61d
Ilya Sinyakin <arqu1t@mail.ru>
2024-07-28 17:29:53 +0300

    Добавил Link событиям

--- src/pages/Home/Events/Events.tsx
+++ src/pages/Home/Events/Events.tsx
@@ -2,14 +2,20 @@ import styles from "./Events.module.css";
 import pagesCss from "../../Pages.module.css";
 import { FC } from "react";
 import EventList from "../../../components/EventList/EventList";
+import { Link } from "react-router-dom";
 
 const Events: FC = () => {
   return (
-    <section className={styles.eventSection}>
+    <section>
       <h2 className={`${pagesCss.sectionTitle} ${pagesCss.sectionTitleLeft}`}>
         Мероприятия
       </h2>
       <EventList quantity={4} />
+      <div className={styles.linkWrapper}>
+        <Link className={styles.link} to="/events">
+          Посмотреть все
+        </Link>
+      </div>
     </section>
   );
 };
