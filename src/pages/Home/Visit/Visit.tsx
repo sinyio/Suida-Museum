7a8623f0811e147f11ac6cb9467eacc0006e6332
Ilya Sinyakin <arqu1t@mail.ru>
2024-07-29 22:16:17 +0300

    test

--- src/pages/Home/Visit/Visit.tsx
+++ src/pages/Home/Visit/Visit.tsx
@@ -2,6 +2,9 @@ import styles from "./Visit.module.css";
 import pagesCss from "../../Pages.module.css";
 import { FC } from "react";
 import { useTranslation } from "react-i18next";
+import { YMaps, Map, Placemark } from "react-yandex-maps";
+
+const apiKey = import.meta.env.VITE_REACT_APP_YANDEX_MAPS_API_KEY;
 
 const Visit: FC = () => {
   const { t } = useTranslation("mainPage");
@@ -11,6 +14,15 @@ const Visit: FC = () => {
       <h2 className={pagesCss.sectionTitle}>{t("visit.title")}</h2>
       <div className={styles.wrapper}>
         <div className={styles.visitLeft}>
+          <YMaps query={{ apikey: apiKey }}>
+            <Map
+              defaultState={{ center: [59.466102, 30.124931], zoom: 15 }}
+              height="100%"
+              width="100%"
+            >
+              <Placemark geometry={[59.466102, 30.124931]} />
+            </Map>
+          </YMaps>
         </div>
         <div className={styles.visitRight}>
           <div className={styles.rightTopWrapper}>
